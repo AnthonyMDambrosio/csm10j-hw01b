c64bb1e452c5a3cdafe800e16400e39d9bf377c9
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-10-05 19:06:50 -0700

    Update Hw01b.java

--- Hw01b.java
+++ Hw01b.java
@@ -1,6 +1,13 @@
+/*
 
+    Programmer  : Anthony D'Ambrosio
+    Date        : 10/5/2015
+    Purpose     : Reads data from a file. finds highs, lows, and the avg. 
+    Limitations : There must be a data file titled stocks.txt inside the same 
+                  folder.
+
+*/
 //package hw01b;
-
 import java.util.*;
 import java.io.*;
 
@@ -10,45 +17,37 @@ public class Hw01b
     
     public static void main( String[] args ) throws FileNotFoundException
     {
-        // Declaring and initalizing the program looping variable
+        // Declaring and initalizing program variables. 
         boolean programQuit = false;
         
-        // Declaring and initializing variables
-            String filename = "stocks.txt",
-                   userMenuChoice;
-            
-            
-            //PrintWriter outFile = new PrintWriter(filename);
-            //Scanner inFile = new Scanner(new FileReader(filename));
-            
-            // Writing bogus data to a file. for debugging purposes.
-            //outFile.print("AAPL 85.90 105.50 ");
-            //outFile.print("MSFT 40.50 80.90");
-            //outFile.close();
-
-         
+        String filename = "stocks.txt",
+               userMenuChoice;
+        
+        // Main loop that keeps the programing running.
         do
         {
+            // Calls the menu and returns the users choice. 
             userMenuChoice = menu();
           
             // Prints the menu choice. for debugging purposes.
             System.out.println("Your choice: " + userMenuChoice);
-
+            
             switch (userMenuChoice)
             {
                 case "1":
                     GetStockStats(filename);
                     break;
                 case "2":
-                    GetStockHigh(filename);
+                    GetStockHighLow(filename, userMenuChoice);
                     break;
                 case "3":
-                    GetStockLow(filename);
+                    GetStockHighLow(filename, userMenuChoice);
                     break;
                 case "c":
                 case "C":
                     System.out.print("Enter a stock filename: ");
                     filename = console.next();
+                    System.out.println(filename);
                     break;
                 case "q":
                 case "Q":
@@ -63,6 +62,7 @@ public class Hw01b
         } while (programQuit != true);
     }
     
+    // Menu 
     public static String menu()
     {
         System.out.println("Enter '1' to get max, min and avg of a stock");
@@ -76,65 +76,129 @@ public class Hw01b
         return userMenuChoice;
     }
     
+    // Finds the lowest, highest, and average of a specific stock.
     public static void GetStockStats ( String filename ) 
             throws FileNotFoundException
     {
+        
+        //Declaring and initializing variables.
         Scanner inFile = new Scanner(new FileReader(filename));
         
-        String userInputStockTicker,
-               stockTicker;
+        String userInputStockTicker = "error",
+               stockTicker,
+               dummyStr;
         
-        Double minPrice,
-               maxPrice,
-               avgPrice;
+        Double minPrice = 0.0,
+               maxPrice = 0.0,
+               avgPrice = 0.0,
+               dummyDouble = 0.0;
+        
+        int stockCount = 0;
                
-     
-        // Gets input from user for which ticker they want.
-        // converts the input to all capital letters.
+        
+        // Gets stock ticker information from user. 
         System.out.print("Enter a stock ticker: ");
         userInputStockTicker = console.next();
+        // Converts user choice to all capitals like in the text file. 
         userInputStockTicker = userInputStockTicker.toUpperCase();
-
-        // Sets the stockTicker variable to the next string in file.
-        stockTicker = inFile.next();
-
-        // This gets us to the data of the ticker entered by user.
-        while (!stockTicker.equals( userInputStockTicker ))
-           stockTicker = inFile.next();
-
-        // Enters data into each of these variables according 
-        // to the ticker entered by the user.
-        minPrice = inFile.nextDouble();
-        maxPrice = inFile.nextDouble();
-        avgPrice = ( ( minPrice + maxPrice ) / 2 );
-
-        // Prints this data to the user. 
-        System.out.printf(stockTicker + " min: %.2f max: %.2f avg: "
-                + "%.2f", minPrice, maxPrice, avgPrice);
-        System.out.println();
-        System.out.println();
-        
-        inFile.close();
-    }
-    
-    public static void GetStockHigh ( String filename ) 
-            throws FileNotFoundException
-    {
-        Scanner inFile = new Scanner(new FileReader(filename));
-        
-        String dummyStr,
-               highestPriceTicker = "null";
-        
-        double dummyDouble,
-               dummyDouble2,
-               highestPriceNum = 0;
-        
- 
+       
+        // Runs until there is nothing left in the file.
         while ( inFile.hasNext() )
         {
+            // If the next string is equal to user choice we step in, otherwise
+            // we dont bother.
+            if ( inFile.next().equals( userInputStockTicker ) )
+            {
+                // Fills a dummy variable. 
+                dummyDouble = inFile.nextDouble();
+                
+                // If this is the first time running into a stock match we 
+                // initalize the min and max. If we dont the next if statmenets
+                // wont catch.
+                if (stockCount == 0)
+                {
+                    minPrice = dummyDouble;
+                    maxPrice = dummyDouble;
+                }
+                
+                // If min is greater than next double, set min to the double. 
+                if (minPrice > dummyDouble)
+                    minPrice = dummyDouble;
+                
+                // if max is less than next double, set max to the double. 
+                if (maxPrice < dummyDouble)
+                    maxPrice = dummyDouble;
+                
+                // average is the sum of all the stocks. we will divide by 
+                // number of that stock later.
+                avgPrice+= dummyDouble;
+                
+                // Holds how many times we run into that specific stock.
+                stockCount++;
+
+            }
+        }
+        
+       //System.out.println("stockCount = " + stockCount);
+       //System.out.println("avg not divided = " + avgPrice);
+
+        // It is impossible for the avg price to be 0 if we ran into that stock
+        // therefore this is our check if the stock actually exists. 
+        if ( avgPrice == 0.0 )
+            System.out.println(userInputStockTicker + " was not found");
+        // else, the stock exists. 
+        else
+        {
+            // Divde the avgprice by how many stocks of that type we ran into.
+            avgPrice = avgPrice / stockCount;
+            // Prints the min, max and avg to the user. 
+            System.out.printf(userInputStockTicker + " min: %.2f max: %.2f avg: "
+                + "%.2f", minPrice, maxPrice, avgPrice );
+            System.out.println();
+        }
+        inFile.close();
+    }
+
+    public static void GetStockHighLow(String filename, String menuChoice) 
+            throws FileNotFoundException
+    {
+        // Declaring and initializing variables 
+        Scanner inFile = new Scanner(new FileReader(filename));
+        
+        String dummyStr = "error",
+               lowestPriceTicker = "error",
+               highestPriceTicker = "error";
+        
+        double dummyDouble = 0.0,
+               lowestPriceNum = 0,
+               highestPriceNum = 0;
+       
+        
+        // Runs for as long as there is data inside the file.
+        for (int c = 0; inFile.hasNext(); c++)
+        {
+            // Variable to hold the next string.
             dummyStr = inFile.next();
+            // Variable to hold the next double. 
             dummyDouble = inFile.nextDouble();
-            dummyDouble = inFile.nextDouble();
+            
+            // If this is the first time the loop is running, we want to 
+            // initializing lowest and ticker, otherwise the next if wont catch.
+            if (c == 0)
+            {
+                lowestPriceTicker = dummyStr;
+                lowestPriceNum = dummyDouble;
+                highestPriceTicker = dummyStr;
+                highestPriceNum = dummyDouble;
+            }
+            
+            // If the current lowest is greater than the next double, set 
+            // lowest equal to that double and get the name of that stock.
+            if (lowestPriceNum > dummyDouble)
+            {
+                lowestPriceTicker = dummyStr;
+                lowestPriceNum = dummyDouble;
+            }
             
             if (highestPriceNum < dummyDouble)
             {
@@ -143,46 +207,127 @@ public class Hw01b
             }
         }
         
+        
+        if ("2".equals( menuChoice ))
+        {
+            System.out.printf(highestPriceTicker + " has highest price of %.2f", 
+                highestPriceNum);
+            System.out.println();
+        }
+        
+        if ("3".equals( menuChoice ))
+        {
+            System.out.printf(lowestPriceTicker + " has lowest price of %.2f", 
+                lowestPriceNum);
+            System.out.println();
+        }
+        System.out.println();
+
+        inFile.close();
+    }
+
+    /* pre extra credit
+    // Finds the highest value of all data in text file. 
+    public static void GetStockHigh ( String filename ) 
+            throws FileNotFoundException
+    {
+        // Declaring and initializing variables.
+        Scanner inFile = new Scanner(new FileReader(filename));
+        
+        String dummyStr,
+               highestPriceTicker = "null";
+        
+        double dummyDouble,
+               highestPriceNum = 0;
+       
+        // Runs for as long as there is data in the file.
+        for (int c = 0; inFile.hasNext(); c++ )
+        {
+            // Holds the next string.
+            dummyStr = inFile.next();
+            // Holds the next double. 
+            dummyDouble = inFile.nextDouble();
+            
+            // If this is the first time the loop is running, we want to 
+            // initalize the variables. Otherwise the next if statement 
+            // wont catch.
+            if (c == 0)
+            {
+                highestPriceTicker = dummyStr;
+                highestPriceNum = dummyDouble;
+            }
+            
+            // If the current highest price is less than the next double 
+            // we make the highest price equal to the new double and get the 
+            // ticker name.
+            if (highestPriceNum < dummyDouble)
+            {
+                highestPriceTicker = dummyStr;
+                highestPriceNum = dummyDouble;
+            }
+
+        }
+        
+        
+        // Prints data out to the user. 
         System.out.printf(highestPriceTicker + " has highest price of %.2f", 
                 highestPriceNum);
         System.out.println();
         System.out.println();
         
-
         inFile.close();
     }
     
-    public static void GetStockLow (String filename) throws FileNotFoundException
+    // Finds the lowest value of all data in text file. 
+    public static void GetStockLow (String filename) 
+            throws FileNotFoundException
     {
+        
+        // Declaring and initializing variables 
         Scanner inFile = new Scanner(new FileReader(filename));
         
         String dummyStr,
-               highestPriceTicker = "error";
+               lowestPriceTicker = "error";
         
         double dummyDouble,
-               dummyDouble2,
-               highestPriceNum = 0;
+               lowestPriceNum = 0;
+       
         
-        while ( inFile.hasNext() )
+        // Runs for as long as there is data inside the file.
+        for (int c = 0; inFile.hasNext(); c++)
         {
+            // Variable to hold the next string.
             dummyStr = inFile.next();
-            dummyDouble = inFile.nextDouble();
-            dummyDouble2 = dummyDouble;
+            // Variable to hold the next double. 
             dummyDouble = inFile.nextDouble();
             
-            if (dummyDouble2 < dummyDouble)
+            // If this is the first time the loop is running, we want to 
+            // initializing lowest and ticker, otherwise the next if wont catch.
+            if (c == 0)
             {
-                highestPriceTicker = dummyStr;
-                highestPriceNum = dummyDouble2;
+                lowestPriceTicker = dummyStr;
+                lowestPriceNum = dummyDouble;
+            }
+            
+            // If the current lowest is greater than the next double, set 
+            // lowest equal to that double and get the name of that stock.
+            if (lowestPriceNum > dummyDouble)
+            {
+                lowestPriceTicker = dummyStr;
+                lowestPriceNum = dummyDouble;
+    
             }
         }
         
-        System.out.printf(highestPriceTicker + " has lowest price of %.2f", 
-                highestPriceNum);
+        System.out.printf(lowestPriceTicker + " has lowest price of %.2f", 
+                lowestPriceNum);
         System.out.println();
         System.out.println();
         
 
         inFile.close();
     }
+    */
+
 }
+
